e1ecf590f840d5bc16de0fcbfbc2897e150798f4
kaushalsatam <kaushalabd17@gmail.com>
2025-02-05 20:19:20 +0530

    Minor changes in colors.lua, themery.lua, which-key.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,10 @@
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    }
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{ "rebelot/kanagawa.nvim" },
 }
