244b2da5762e0d60d73ade6d0472176f0d9a27df
kaushalsatam <kaushalabd17@gmail.com>
2025-02-24 23:39:49 +0530

    add some keybindings to flutter-tools

--- lua/plugins/lsps/flutter-tools.lua
+++ lua/plugins/lsps/flutter-tools.lua
@@ -5,5 +5,17 @@ return {
 		"nvim-lua/plenary.nvim",
 		"stevearc/dressing.nvim", -- optional for vim.ui.select
 	},
-	config = true,
+	config = function()
+		require("flutter-tools").setup({
+			vim.keymap.set("n", "<leader>Fd", ":FlutterDevices<CR>", { desc = "[F]lutter [D]evices" }),
+			vim.keymap.set("n", "<leader>Fe", ":FlutterEmulators<CR>", { desc = "[F]lutter [E]mulators" }),
+			vim.keymap.set("n", "<leader>Frs", ":FlutterRestart<CR>", { desc = "[F]lutter [R]e[s]tart" }),
+			vim.keymap.set("n", "<leader>Frl", ":FlutterReload<CR>", { desc = "[F]lutter [R]e[l]oad" }),
+			vim.keymap.set("n", "<leader>Frn", ":FlutterRun<CR>", { desc = "[F]lutter [R]u[n]" }),
+			vim.keymap.set("n", "<leader>Fq", ":FlutterQuit<CR>", { desc = "[F]lutter [Q]uit" }),
+			vim.keymap.set("n", "<leader>Fc", ":FlutterCopyProfilerUrl<CR>", { desc = "[F]lutter [C]opyProfileUrl" }),
+		})
+		return true
+	end,
+	-- config = true,
 }
